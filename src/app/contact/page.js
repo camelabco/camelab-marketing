43d8c2b97bc255fd7de7f20caad3f17f6d9116be
jay74123 <jatin@closedigit.com>
2024-05-04 12:24:07 +0530

    change

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -1,9 +1,11 @@
 import Header from '../component/header'
+import Footer from '../component/footer';
 export default function Contact() {
   return (
     <>
         <Header />
         <h1>contact</h1>
+        <Footer/>
     </>
   );
 }
