0e0be4580f921918e20c245c13e2e6a29a3054d6
DivishaM <divisha@closedigit.com>
2024-05-08 17:02:56 +0530

    about-us page ltr

--- src/app/layout.js
+++ src/app/layout.js
@@ -10,7 +10,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body dir="rtl">{children}</body>
+      <body>{children}</body>
     </html>
     // dir="rtl"
   );
